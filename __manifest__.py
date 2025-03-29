19a58f39507a1fd9fead9174bcf47b9beb0ff18c
Antonio Queb <antonioqueb@alphaqueb.online>
2025-03-29 06:43:17 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Cambio de nombre a Órdenes de Servicio en Ventas",
+    "version": "1.0",
+    "summary": "Reemplaza 'Presupuestos' por 'Órdenes de Servicio' en el módulo de ventas",
+    "author": "Alphaqueb Consulting",
+    "category": "Sales",
+    "depends": ["sale"],
+    "data": [
+        "views/orden_servicio_menu.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
